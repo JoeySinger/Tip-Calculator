fd93293721e6d801d777523e0736478c925c3a88
michael singer <michaelsinger@Macintosh.local>
2017-01-06 02:50:13 -0600

    added .gitignore .gitattributes and my code

--- Tip Calc/ViewController.swift
+++ Tip Calc/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var smileyLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calcTip(_ sender: AnyObject) {
+        let tipPercents = [0.18, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercents[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        if tipControl.selectedSegmentIndex == 0 {
+            smileyLabel.text = ":("
+        }
+        else if tipControl.selectedSegmentIndex == 1 {
+            smileyLabel.text = ":|"
+        }
+        else {
+            smileyLabel.text = ":)"
+        }
+    }
 }
 
